437cbd4bd60c5c8b202615fc66cd812501f046fa
Mykell404 <milebamicheal@gmail.com>
2022-09-09 23:02:16 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * Return: Value return is 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
