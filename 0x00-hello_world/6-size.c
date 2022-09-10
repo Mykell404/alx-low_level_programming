eb41269733e17475cc4f202d22a39469f0877e67
Mykell404 <milebamicheal@gmail.com>
2022-09-09 23:42:55 -0700

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ long long int d;
 float e;
 
 
-printf("Size of a char: %lu byte(s)", (unsigned long)sizeof(a));
-printf("Size of a int: %lu byte(s)", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)", (unsigned long)sizeof(c));
-printf("Size of a long long int: %lu byte(s)", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(e));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 
 return (0);
 }
