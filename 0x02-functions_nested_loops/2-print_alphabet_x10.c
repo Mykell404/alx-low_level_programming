13af66d3feb67d8e72e87b12a162ea2028842efe
Mykell404 <milebamicheal@gmail.com>
2022-09-14 12:27:48 -0700

    fix

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int a = 1;
+
+	while (a <= 10)
+	{
+		char alphabet = 'a';
+
+		while (alphabet <= 'z')
+		{
+			_putchar(alphabet);
+			alphabet++;
+		}
+		a++;
+	}
+}
