ce52ec46d9ff724129761de491f2b7fbbe2ec3a6
Mykell404 <milebamicheal@gmail.com>
2022-09-19 03:46:32 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - check the code
+ * @n: a pointer to n
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+### Pointers, Strings and Array
