63359765a8e47733221847b3d5c151b499ba9793
Mykell404 <milebamicheal@gmail.com>
2022-09-21 11:41:26 -0700

    initial

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
+ * Return: destination
+ */
+char *_strcat(char *dest, char *src)
+{
+	int count, count2 = 0;
+	while (dest[count] != '\0')
+	{
+		count++;
+	}
+	while (count2 >= 0)
+	{
+		dest[count] = src[count2];
+		if (src[count2] == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
+}
