87904969171620484e0609f0fd2a77bcc61ab908
Mykell404 <milebamicheal@gmail.com>
2022-09-29 12:29:33 +0100

    recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* _sqrt_recursion - main function
+* @n: int n
+* Return: integer
+*/
+int _sqrt_recursion(int n)
+{
+return (_sqrt(n, 1));
+}
+/**
+* _sqrt - _sqrt_recursion
+* @n: integer parameter
+* @i: integer parameter
+* Return: sqrt
+*/
+int _sqrt(int n, int i)
+{
+if (n < 0)
+return (-1);
+if ((i * i) > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (_sqrt(n, i + 1));
+}
