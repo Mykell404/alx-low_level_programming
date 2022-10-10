7065031b6366a772da5bb9058c24f148cad5fe89
Mykell404 <milebamicheal@gmail.com>
2022-10-10 12:43:56 +0100

    fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,12 +10,12 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		return;
+	if (d != NULL)
+		d->name = name;
+	d->age = age;
+	d->owner = owner;
 	else
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		return;
 	}
 }
