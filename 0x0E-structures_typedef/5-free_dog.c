93864c75b53ed00a5f7a75fede72a6679b1f1240
Mykell404 <milebamicheal@gmail.com>
2022-10-10 12:37:35 +0100

    structure

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - free memory
+ *@d: array
+ * Description: Free memory for struct
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
