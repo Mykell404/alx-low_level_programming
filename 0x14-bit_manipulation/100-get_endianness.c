ab99ff6d890520fdcde09602d332e0737840a7f3
Mykell404 <milebamicheal@gmail.com>
2022-10-27 22:19:54 +0100

    fix

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -7,11 +7,11 @@
  */
 int get_endianness(void)
 {
-  unsigned int x;
-  char *c;
+	unsigned int x;
+	char *c;
 
-  x = 1;
-  c = (char *)&x;
+	x = 1;
+	c = (char *)&x;
 
-  return ((int)*c);
+	return ((int)*c);
 }
